38132af34754f7715c2dd6e5dc4320adb373781f
Eo Yeonjin <wks1211@gmail.com>
2024-07-30 11:58:34 +0900

    Chore: Set the back-end

---
+++ src/test/java/BookmyBook/bmb/BmbApplicationTests.java
@@ -0,0 +1,13 @@
+package BookmyBook.bmb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
