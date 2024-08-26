a99f841a585c6acaa9c63d1ea96440b2e54ab8b2
HauOmochikaeri <bareulgod2466@gmail.com>
2024-08-26 16:12:59 +0900

    Feat: add bookApplicationAPI - BookDTOの図書登録の為の要素の追加。 - BookServiceのAPIを追加。 Related to: #15

--- src/main/java/BookmyBook/bmb/api/BookApiController.java
+++ src/main/java/BookmyBook/bmb/api/BookApiController.java
@@ -1,26 +1,38 @@
 package BookmyBook.bmb.api;
 
+import BookmyBook.bmb.domain.Book;
+import BookmyBook.bmb.domain.BookStatus;
 import BookmyBook.bmb.response.ApiResponse;
 import BookmyBook.bmb.response.BookResponse;
 import BookmyBook.bmb.response.ExceptionResponse;
+import BookmyBook.bmb.response.dto.BookDto;
 import BookmyBook.bmb.service.BookService;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class BookApiController {
 
     private final BookService bookService;
 
     //도서 목록 조회
     @GetMapping("/books")
-    @PreAuthorize("hasRole('User') or hasRole('Admin')")
+    @PreAuthorize("hasRole('User') or hasRole('Admin')") // 참고
     public ResponseEntity<?> getBookList(
             HttpServletRequest request,
             @RequestParam(value = "page", defaultValue = "1") int page,
@@ -49,4 +61,53 @@ public class BookApiController {
             throw new ExceptionResponse(404, "도서 목록 조회 실패", "FAIL_TO_LOAD");
         }
     }
+
+    @PostMapping("/books/insert") // 도서 추가
+    @PreAuthorize("hasRole('Admin')")
+    public ResponseEntity<?> insertBook(@RequestBody CreateBookRequest request){
+        Book book = new Book();
+        book.setId(request.id);
+        book.setIsbn(request.isbn);
+        book.setTitle(request.title);
+        book.setAuthor_name(request.author_name);
+        book.setPublisher_name(request.publisher_name);
+        book.setThumbnail(request.thumbnail);
+        book.setDescription(request.description);
+        book.setPublished_date(request.published_date);
+        book.setCreated_at(LocalDateTime.now());
+        book.setStatus(request.status);
+
+        log.info("Request: {}", request);
+        log.info("CreatedTime : {}", book.getCreated_at());
+        Book insert = bookService.insert(book);
+        BookDto bookDto = new BookDto(book.getIsbn(), book.getId(), book.getTitle(), book.getThumbnail(), book.getAuthor_name(),
+                book.getPublisher_name(), book.getStatus(), book.getDescription(),
+                book.getPublished_date(), book.getCreated_at());
+
+        return ResponseEntity.ok(new ApiResponse(200, "도서 추가 성공", bookDto));
+    }
+/*
+    @PostMapping("/books/view") // 도서 한 권 상세 조회
+    @PreAuthorize("hasRole('User') or hasRole('Admin')")
+    public ResponseEntity<?> viewBook(@RequestParam(value = "id") int id){
+        // 도서 갖고 오기
+        bookService.view(id);
+
+        return ResponseEntity.ok(new ApiResponse(200, "도서 가져오기 성공", bookDto));
+    }
+*/
+    @Data
+    static class CreateBookRequest {
+        private String isbn;
+        private long id;
+        private String title;
+        private String author_name;
+        private String publisher_name;
+        private String thumbnail;
+        private String description;
+        private LocalDate published_date;
+        private LocalDateTime created_at;
+        private BookStatus status;
+    }
+
 }
--- src/main/java/BookmyBook/bmb/response/dto/BookDto.java
+++ src/main/java/BookmyBook/bmb/response/dto/BookDto.java
@@ -4,6 +4,13 @@ import BookmyBook.bmb.domain.BookStatus;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+
+// 도서 목록 : 조히할 따랑은 다름
+
+// 도서 목록과 도서 추가를 하나로 통일
 @Getter @Setter
 public class BookDto {
 
@@ -15,6 +22,12 @@ public class BookDto {
     private BookStatus status;
     private Long wish_count;
     private boolean wished;
+
+    //선우`s dto
+    private long id;
+    private String description;
+    private LocalDateTime created_at;
+    private LocalDate published_date;
 
     public BookDto(String isbn, String title, String thumbnail, String author_name, String publisher_name, BookStatus status,
                    Long wish_count, boolean wished) {
@@ -28,4 +41,21 @@ public class BookDto {
         this.wish_count = wish_count;
         this.wished = wished;
     }
+
+    // 선우`s 생성자 - 도서 추가
+    public BookDto(String isbn, long id, String title, String thumbnail, String author_name,
+                   String publisher_name, BookStatus status, String description,
+                   LocalDate published_date, LocalDateTime created_at) {
+        this.isbn = isbn;
+        this.id = id;
+        this.title = title;
+        this.thumbnail = thumbnail;
+        this.author_name = author_name;
+        this.publisher_name = publisher_name;
+        this.status = status;
+        this.description = description;
+        this.published_date = published_date;
+        this.created_at = created_at;
+    }
+
 }
