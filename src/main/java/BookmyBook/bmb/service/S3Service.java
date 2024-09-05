910c80e24113da7e024fce02491882efaf25437e
Eo Yeonjin <wks1211@gmail.com>
2024-09-05 13:50:16 +0900

    Feat: add image upload logic in insertBook(admin) API
    - add image upload logic
    - modify to hide important information at application.yml (token, key)
    - add the dependency for upload s3 server
    
    related to #93

---
+++ src/main/java/BookmyBook/bmb/service/S3Service.java
@@ -0,0 +1,43 @@
+package BookmyBook.bmb.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectResponse;
+import software.amazon.awssdk.core.sync.RequestBody;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
+
+@Service
+public class S3Service {
+
+    private final S3Client s3Client;
+
+    @Value("${aws.s3.bucket}")
+    private String bucketName;
+
+    public S3Service(S3Client s3Client) {
+        this.s3Client = s3Client;
+    }
+
+    public String uploadFile(MultipartFile file) throws IOException {
+        String fileName = UUID.randomUUID() + "-" + file.getOriginalFilename();
+
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(fileName)
+                .build();
+
+        try (InputStream inputStream = file.getInputStream()) {
+            PutObjectResponse response = s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(inputStream, file.getSize()));
+            System.out.println("File uploaded successfully. ETag: " + response.eTag());
+        }
+
+        // Return the URL of the uploaded file
+        return String.format("https://%s.s3.%s.amazonaws.com/%s", bucketName, s3Client.serviceClientConfiguration().region().id(), fileName);
+    }
+}
