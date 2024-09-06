64ee72e1d61bc77179d13794095d660286a1e179
Eo Yeonjin <wks1211@gmail.com>
2024-09-06 17:33:07 +0900

    Feat: add logic to delete image at the server
    - add the image delete logic
    
    related to #116

--- src/main/java/BookmyBook/bmb/service/S3Service.java
+++ src/main/java/BookmyBook/bmb/service/S3Service.java
@@ -7,6 +7,8 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.core.exception.SdkException;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.DeleteObjectResponse;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectResponse;
 import software.amazon.awssdk.core.sync.RequestBody;
@@ -28,6 +30,7 @@ public class S3Service {
         this.s3Client = s3Client;
     }
 
+    //이미지 업로드
     public String uploadFile(MultipartFile file) throws IOException {
         String fileName = UUID.randomUUID() + "-" + file.getOriginalFilename();
 
@@ -45,5 +48,27 @@ public class S3Service {
 
         // Return the URL of the uploaded file
         return String.format("https://%s.s3.%s.amazonaws.com/%s", bucketName, s3Client.serviceClientConfiguration().region().id(), fileName);
+    }
+
+    //이미지 삭제
+    public void deleteFile(String fileUrl) {
+        String fileName = extractFileNameFromUrl(fileUrl);
+
+        DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
+                .bucket(bucketName)
+                .key(fileName)
+                .build();
+
+        try {
+            DeleteObjectResponse response = s3Client.deleteObject(deleteObjectRequest);
+            System.out.println("File deleted successfully. VersionId: " + response.versionId());
+        } catch (SdkException e) {
+            throw new ExceptionResponse(500, "이미지 삭제 실패", "FAILED_TO_DELETE");
+        }
+    }
+
+    //url에 삭제할 추출
+    private String extractFileNameFromUrl(String fileUrl) {
+        return fileUrl.substring(fileUrl.lastIndexOf('/') + 1);
     }
 }
