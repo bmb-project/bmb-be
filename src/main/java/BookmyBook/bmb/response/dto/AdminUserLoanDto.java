1e70c999a532f2616c4d4fdc0998cd8f3e7e46ed
EoYeonjin <wks1211@gmail.com>
2024-11-18 14:32:26 +0900

    Refactor : modify AdminUserLaonList API (#127)
    - correct the error 500 if parameter contain keyword
    - add the created_at in AdminUsersDto
    - add the id in AdminUserLoanDto
    
    related to #126

--- src/main/java/BookmyBook/bmb/response/dto/AdminUserLoanDto.java
+++ src/main/java/BookmyBook/bmb/response/dto/AdminUserLoanDto.java
@@ -8,6 +8,7 @@ import java.time.LocalDateTime;
 
 @Getter @Setter
 public class AdminUserLoanDto {
+    private Long id;
     private String isbn;
     private String title;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
@@ -16,7 +17,8 @@ public class AdminUserLoanDto {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
     private LocalDateTime return_at;
 
-    public AdminUserLoanDto(String isbn, String title, LocalDateTime loan_at, LocalDateTime return_at) {
+    public AdminUserLoanDto(Long id, String isbn, String title, LocalDateTime loan_at, LocalDateTime return_at) {
+        this.id = id;
         this.isbn = isbn;
         this.title = title;
         this.loan_at = loan_at;
--- src/main/java/BookmyBook/bmb/response/dto/AdminUsersDto.java
+++ src/main/java/BookmyBook/bmb/response/dto/AdminUsersDto.java
@@ -1,19 +1,26 @@
 package BookmyBook.bmb.response.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter @Setter
 public class AdminUsersDto {
     private String user_id;
     private String nickname;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
+    private LocalDateTime created_at;
+
     private List<AdminUserLoanDto> loans;
 
-    public AdminUsersDto(String user_id, String nickname, List<AdminUserLoanDto> loans) {
+    public AdminUsersDto(String user_id, String nickname, LocalDateTime created_at, List<AdminUserLoanDto> loans) {
         this.user_id = user_id;
         this.nickname = nickname;
+        this.created_at = created_at;
         this.loans = loans;
     }
 }
